d5ce0a42527b35659a3a58e4b396336c55240f20
Erik Herrmann <erikherrmann1@gmx.de>
2024-01-08 02:13:21 +0100

    Add Home Screen, Drawer, Routing and Settings Menu

---
+++ lib/drawer.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:fuzzy_guacamole/main.dart';
+import 'package:fuzzy_guacamole/settings/start.dart';
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+
+}
+
+class _MyDrawerState extends State<MyDrawer>{
+
+  bool checkboxValue1 = false;
+  bool checkboxValue2 = false;
+  bool checkboxValue3 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black26,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 8, bottom: 8),
+                  child: Text(
+                    'Flutter Kalender',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.person,
+                        size: 40,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        'Erik',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Start'),
+            onTap: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.calendar_view_day),
+            title: const Text('Tag'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.calendar_view_week_sharp),
+            title: const Text('Woche'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.calendar_view_month_sharp),
+            title: const Text('Monat'),
+            onTap: () {},
+          ),
+          const Divider(color: Colors.grey,),
+          CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            //checkColor: ,
+            title: const Text('Label 1'),
+            value: checkboxValue1,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue1 = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            //checkColor: ,
+            title: const Text('Label 2'),
+            value: checkboxValue2,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue2 = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            //checkColor: ,
+            title: const Text('Label 3'),
+            value: checkboxValue3,
+            onChanged: (bool? value) {
+              setState(() {
+                checkboxValue3 = value!;
+              });
+            },
+          ),
+          const Divider(color: Colors.grey,),
+          ListTile(
+            leading: const Icon(Icons.settings_outlined),
+            title: const Text('Einstellung'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsMenu()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.help_outline_rounded),
+            title: const Text('Hilfe'),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:fuzzy_guacamole/drawer.dart';
+import 'package:fuzzy_guacamole/settings/start.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    final padding = MediaQuery.of(context).padding;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('This is a snackbar')));
-            },
-          )
-        ],
+        title: const Text('Flutter Kalender'),
       ),
-      drawer: const Drawer(
-        child: Padding(
-          padding: EdgeInsets.zero,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 100,
-                width: double.infinity,
-                child:DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'Drawer Header',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.message),
-                title: Text('Message'),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: MyDrawer(),
     );
   }
 }
